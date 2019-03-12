d774bc45198ade036c99fb32e34a3bc0bfed83d2
Mateusz Starakiewicz <ms.starakiewicz@gmail.com>
2019-03-13 00:31:50 +0100

    fix idex.js

--- index.js
+++ index.js
@@ -2,44 +2,20 @@ var http = require('http');
 var fs = require('fs');
 var server = http.createServer();
 
-/*
-server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.method === 'GET' && request.url === '/hello') {
-        response.write('<h1>Hello World!</h1>');
-            response.end();
-    } else {
-            response.statusCode = 404;
-            response.write('<h1>404: Zła ścieżka!</h1>');
-            response.end();
-    }
-});
 
-*/
-
-server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.method === 'GET' && request.url === '/treasure') {
-        fs.readFile('index.html', 'utf-8', function(err, data) {
-   		 response.write(data);
-   		 response.end();
-		});
+server.on("request", function(request, response) {
+    if (request.method === "GET" && request.url === "/treasure") {
+      console.log("robie to");
+      fs.readFile("index.html", "utf-8", function(err, data) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(data);
+      });
     } else {
-            //response.statusCode = 404;
-            fs.readFile('./img/404.jpg', 'image/jpeg', function(err, data) {
-   			response.write('<img src="' + data + '" alt=" error 404">');
-   		 	response.end();
-			});
+      fs.readFile("./img/404.jpg", "binary", function(err, data) {
+        response.writeHead(404, { "Content-Type": "image/jpeg" });
+        response.end(data, "binary");
+      });
     }
 });
 
 server.listen(8080);
-
-/*
-
-fs.readFile('./treasure',  function(err, data) {
-    response.write('<img src="' + data + '" alt="treasure">');
-    response.end();
-}
-
-*/
